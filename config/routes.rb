e76e39b1df94c95072db613e49df9cda9140c35a
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-08-16 10:32:07 -0400

    Games controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/index'
+
+  get 'games/show'
+
   root 'users#new'
 
   get '/login' => 'sessions#new', as: 'login'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816031756) do
+ActiveRecord::Schema.define(version: 20170816141849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.integer "score"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 20170816031756) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "games", "users"
 end
